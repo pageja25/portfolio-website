59da33e23ea45c701a7aed95920e9fec30a6f9f8
pageja25 <43889489+pageja25@users.noreply.github.com>
2024-08-11 12:14:53 -0400

    Changed favicon

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <div class="relative flex-col container mt-24 mx-auto py-4 px-12">
+      <div className="relative flex-col container mt-24 mx-auto py-4 px-12">
         <HeroSection />
         <Experience />
         <EmailSection />
